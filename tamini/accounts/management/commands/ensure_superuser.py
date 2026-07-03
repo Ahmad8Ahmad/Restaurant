ac779ff8cb54798c2163bf947bc08471e433c322
Ahmad Almasri <ahmad0944043511@gmail.com>
2026-07-03 11:35:29 +0200

    fix: ensure_superuser handles duplicate username gracefully

--- tamini/accounts/management/commands/ensure_superuser.py
+++ tamini/accounts/management/commands/ensure_superuser.py
@@ -1,5 +1,6 @@
 from django.core.management.base import BaseCommand
 from django.contrib.auth import get_user_model
+from django.db import IntegrityError
 
 User = get_user_model()
 
@@ -12,22 +13,39 @@ class Command(BaseCommand):
         password = 'admin123'
         username = 'admin'
 
-        user, created = User.objects.get_or_create(
-            email=email,
-            defaults={
-                'username': username,
-                'is_staff': True,
-                'is_superuser': True,
-            }
-        )
-
-        if created:
-            user.set_password(password)
-            user.save()
-            self.stdout.write(self.style.SUCCESS(f'Superuser "{email}" created'))
-        else:
+        try:
+            user = User.objects.get(email=email)
             user.is_staff = True
             user.is_superuser = True
             user.set_password(password)
             user.save()
             self.stdout.write(self.style.SUCCESS(f'Superuser "{email}" updated'))
+            return
+        except User.DoesNotExist:
+            pass
+
+        if not User.objects.filter(username=username).exists():
+            User.objects.create_superuser(
+                email=email,
+                username=username,
+                password=password,
+            )
+            self.stdout.write(self.style.SUCCESS(f'Superuser "{email}" created'))
+            return
+
+        for i in range(1, 100):
+            alt_username = f'{username}{i}'
+            if not User.objects.filter(username=alt_username).exists():
+                User.objects.create_superuser(
+                    email=email,
+                    username=alt_username,
+                    password=password,
+                )
+                self.stdout.write(
+                    self.style.SUCCESS(
+                        f'Superuser "{email}" created with username "{alt_username}"'
+                    )
+                )
+                return
+
+        self.stdout.write(self.style.ERROR('Could not create superuser'))
